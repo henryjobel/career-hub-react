6016d7b105b33f6360ec1cf8530a062491132b44
henryjobel <jhobelsaha@gmail.com>
2023-09-22 03:21:25 +0600

    header created done and path and react router setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import Jobs from './Components/Jobs/Jobs';
+import Blogs from './Components/Blogs/Blogs';
+import AppliedJobs from './Components/Appliedjobs/AppliedJobs.Jsx';
+import Statistics from './Components/Statistics/Statistics.Jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/jobs",
+        element: <Jobs></Jobs>
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>
+       
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>
+      }
+      
+    ]
+    
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
